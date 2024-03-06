3fb81d57f62d02356f9d2e4661e0ce1337ef25d9
Hyeonsu99 <johyeonsu08@gmail.com>
2024-03-06 23:56:33 +0900

    HP 바 추가

--- UnityProject/ToTheAbyss/Assets/Monster.cs
+++ UnityProject/ToTheAbyss/Assets/Monster.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class Monster : MonoBehaviour
@@ -9,22 +11,41 @@ public class Monster : MonoBehaviour
 
     public UnityAction OnDeath;
 
+    public Slider HpBar;
+
+    public TextMeshProUGUI HpText;
+
+
     // Start is called before the first frame update
     void Start()
     {
         var monsterSpawner = FindObjectOfType<MonsterSpawner>();
 
-        if(monsterSpawner != null)
+        if (monsterSpawner != null)
         {
             if(monsterSpawner.Count <= 0)
             {
                 Health = 100;
+
+                
             }
             else
             {
                 Health = 100 + (int)(100 * monsterSpawner.Count * 0.1f);
             }
         }
+
+        HpBar.minValue = 0;
+        HpBar.maxValue = Health;
+
+        HpBar.value = Health;
+    }
+
+    private void Update()
+    {
+        HpBar.value = Health;
+
+        HpText.text = string.Format(Health.ToString() + " / " + HpBar.maxValue);
     }
 
     public void TakeDamage(int Damage)
