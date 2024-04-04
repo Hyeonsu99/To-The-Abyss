2a1600611c0ca93d790bad728edb21d24ea888cc
cho <hyeonsu99@dcarrick.co.kr>
2024-04-04 18:00:50 +0900

    스크립터블 추가

---
+++ UnityProject/ToTheAbyss/Assets/Script/ScriptableObjects/MonsterData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MonsterData : ScriptableObject
+{
+    public float currentHp;
+    public float MaxHp;
+}
---
+++ UnityProject/ToTheAbyss/Assets/Script/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public float damage;
+    public float autodamage;
+
+    public float damageLevel;
+    public float autodamageLevel;
+}
