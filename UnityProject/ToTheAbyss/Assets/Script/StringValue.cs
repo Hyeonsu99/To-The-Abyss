961293b1e805fe9fb96b396836ce1fc36063ef88
cho <hyeonsu99@dcarrick.co.kr>
2024-04-29 15:30:45 +0900

    씬 이름 변경(StartScene -> MainScene, MainScene -> GameScene), StringValue 수정
    그에 따른 스크립트 변경

--- UnityProject/ToTheAbyss/Assets/Script/StringValue.cs
+++ UnityProject/ToTheAbyss/Assets/Script/StringValue.cs
@@ -2,9 +2,9 @@ public static class StringValue
 {
     public static class Scene
     {
-        public const string startScene = "StartScene";
+        public const string startScene = "MainScene";
         public const string miniGameScene = "MiniGameScene";
-        public const string mainScene = "MainScene";
+        public const string gameScene = "GameScene";
         public const string loadingScene = "LoadingScene";
     }
 }
