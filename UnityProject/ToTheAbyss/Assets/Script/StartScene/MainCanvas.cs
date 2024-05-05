be6f28f10f2b7872e337a40c4354133388b7747f
Hyeonsu99 <johyeonsu08@gmail.com>
2024-05-06 01:21:51 +0900

    start 화면 수정 중 ( 백그라운드 이미지 추가 , 글자 페이드 인 아웃 추가 )

--- UnityProject/ToTheAbyss/Assets/Script/StartScene/MainCanvas.cs
+++ UnityProject/ToTheAbyss/Assets/Script/StartScene/MainCanvas.cs
@@ -2,13 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class MainCanvas : MonoBehaviour
+public class MainCanvas : MonoBehaviour, IPointerDownHandler
 {
+    [SerializeField] private Image _backgroundimg;
 
+    [SerializeField] private Text _logotext;
+
+    [SerializeField] private Text _starttext;
+    
     public void OnClick()
     {
-        SceneManager.LoadScene(StringValue.Scene.loadingScene);
+        
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeLogoText());
+    }
+
+    IEnumerator FadeLogoText()
+    {
+        Color color = _logotext.color;
+
+        while(_logotext.color.a < 1)
+        {
+            color.a += Time.deltaTime * 1.5f;
+
+            _logotext.color = color;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        while (_logotext.color.a >= 0)
+        {
+            color.a -= Time.deltaTime * 1.5f;
+
+            _logotext.color = color;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        _backgroundimg.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeInOutStartText()
+    {
+        yield return null;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_backgroundimg.gameObject.activeSelf == false && Input.touchCount > 0)
+        {
+            SceneManager.LoadScene(StringValue.Scene.loadingScene);
+        }
     }
 }
